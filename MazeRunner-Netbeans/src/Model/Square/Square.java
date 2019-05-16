c387d0dd7e81d502a0707abebf0f30926b40ed68
Victor BRIERE <victorbriere@yahoo.fr>
2019-05-16 14:05:07 +0200

    class Apple & Square implementation

--- MazeRunner-Netbeans/src/Model/Square/Square.java
+++ MazeRunner-Netbeans/src/Model/Square/Square.java
@@ -5,10 +5,42 @@
  */
 package Model.Square;
 
+import java.awt.Image;
+
 /**
  *
  * @author asus
  */
 public class Square {
+    
+    protected int x;
+    protected int y;
+    
+    protected int id;
+    protected Image image;
+    
+    public Square(){     
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+    
+    public Square(int _id, Image _image){
+        this.id = _id;
+        this.image = _image;
+    }
     
 }
